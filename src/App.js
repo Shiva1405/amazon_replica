9fabf39ab0b4e53860d58300b89b9826a376cd5b
Shiva1405 <71342542+Shiva1405@users.noreply.github.com>
2021-06-12 10:48:43 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home";
+function App() {
+return (
+  <div className="app">
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Header />
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  </div>
+);
+}
+export default App;
